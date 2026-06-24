5a4f318ba0a48fa39706073b30bcd00851327cbf
zroquec <zroquec@unjbg.edu.pe>
2026-06-24 10:17:37 -0500

    Avance de registro de libros

--- Inicio.cpp
+++ Inicio.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+struct Libro{
+    string titulo;
+    string autor;
+    string editorial;
+    int anioPublicacion;
+};
+
 int main(){
+    int n;
+    Libro libros[100];
+    cout<<"Ingrese la cantidad de libros: ";
+    cin>>n;
+    cin.ignore();
+
+    for (int i=0; i<n; i++){
+        cout<<"\nLibro "<<i+1<<endl;
+        cout<<"Titulo: ";
+        getline(cin, libros[i].titulo);
+        cout<<"Autor: ";
+        getline(cin, libros[i].autor);
+        cout<<"Editorial: ";
+        getline(cin, libros[i].editorial);
+    }
 
 
-    
 }
