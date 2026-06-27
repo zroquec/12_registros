b9c11f38c09d0aa056d78dbb4555561d15a711d6
zroquec <zroquec@unjbg.edu.pe>
2026-06-27 09:13:05 -0500

    Propuesto1: Tercer avance

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -15,4 +15,15 @@ int main(){
     cin>>n;
     cin.ignore();
     
+    for(int i=0; i<n; i++){
+        cout<<"\nEmpleado "<<i+1<<endl;
+        cout<<"Nombre: ";
+        getline(cin, E[i].Nombre);
+        cout<<"Sexo: ";
+        getline(cin, E[i].Sexo);
+        cout<<"Sueldo: ";
+        cin>>E[i].Sueldo;
+        cin.ignore();
+    }
+    
 }
