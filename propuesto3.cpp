f9d80053a33f69caa5b390fd0179d9256881812d
zroquec <zroquec@unjbg.edu.pe>
2026-06-29 13:12:07 -0500

    Propuesto3: Cuarto avance

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -28,4 +28,19 @@ int main(){
         cin>>P[i].anio;
         cin.ignore();
     }
+
+    do{
+        cin>>mes;
+         if (mes!=0){
+            for(int i=0; i<n; i++){
+                if(mes==P[i].mes){
+                    cout<<"\nPersona "<<i+1<<endl;
+                    cout<<"Nombre: "<<P[i].nombre<<endl;
+                    cout<<"Dia de nacimiento: "<<P[i].dia<<endl;
+                    cout<<"Mes de nacimiento: "<<P[i].mes<<endl;
+                    cout<<"Anio de nacimiento: "<<P[i].anio<<endl;
+                }
+            }
+        }
+    }while(mes!=0);
 }
