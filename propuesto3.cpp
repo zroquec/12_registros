6f6c74e5d42cf9873c0ff9c3788603da82211ee9
zroquec <zroquec@unjbg.edu.pe>
2026-06-29 12:58:29 -0500

    Propuesto3: Tercer avance

--- propuesto3.cpp
+++ propuesto3.cpp
@@ -15,5 +15,17 @@ int main(){
     cout<<"Ingrese la cantidad de personas a registrar: ";
     cin>>n;
     cin.ignore();
-    
+
+    for(int i=0; i<n; i++){
+        cout<<"\nPersona "<<i+1<<endl;
+        cout<<"Nombre: ";
+        getline(cin, P[i].nombre);
+        cout<<"Dia de nacimiento: ";
+        cin>>P[i].dia;
+        cout<<"Mes de nacimiento: ";
+        cin>>P[i].mes;
+        cout<<"Anio de nacimiento: ";
+        cin>>P[i].anio;
+        cin.ignore();
+    }
 }
