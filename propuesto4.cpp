b11c806e0a4324166472d8259bf71691e097e5b6
zroquec <zroquec@unjbg.edu.pe>
2026-06-29 20:26:58 -0500

    Propuesto4: Terminado

--- propuesto4.cpp
+++ propuesto4.cpp
@@ -11,7 +11,7 @@ struct Atleta{
 int main(){
     Atleta A[1000];
     string pais;
-    int n,max=0;
+    int n,max=-1;
     cout<<"Ingrese la cantidad de atletas: ";
     cin>>n;
     cin.ignore();
@@ -29,11 +29,14 @@ int main(){
         cin.ignore();
     }
     int cont=0;
+    bool encontrado = false;
     cout<<"\nIngrese el pais a buscar: ";
     getline(cin, pais);
 
     for(int i=0; i<n; i++){
         if(pais==A[i].pais){
+            encontrado = true;
+
             cout<<"\nAtleta "<<i+1<<endl;
             cout<<"Nombre: "<<A[i].nombre<<endl;
             cout<<"Pais: "<<A[i].pais<<endl;
@@ -46,9 +49,15 @@ int main(){
             }
         }
     }
-    cout<<"\nAtleta con mayor numero de medallas\n";
-    cout<<"Nombre: "<<A[cont].nombre<<endl;
-    cout<<"Pais: "<<A[cont].pais<<endl;
-    cout<<"Disciplina: "<<A[cont].disciplina<<endl;
-    cout<<"Nro de medallas: "<<A[cont].medalla<<endl;
+    if(encontrado){
+        cout<<"\nAtleta con mayor numero de medallas\n";
+        cout<<"Nombre: "<<A[cont].nombre<<endl;
+        cout<<"Pais: "<<A[cont].pais<<endl;
+        cout<<"Disciplina: "<<A[cont].disciplina<<endl;
+        cout<<"Nro de medallas: "<<A[cont].medalla<<endl;
+    }
+    else{
+        cout<<"\nNo se encontraron atletas de ese pais."<<endl;
+    }
+    return 0;
 }
