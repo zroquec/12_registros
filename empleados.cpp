7cefbce0984a6119e0ec011da2784b8643327693
zroquec <zroquec@unjbg.edu.pe>
2026-07-01 10:05:26 -0500

    Empleado: Primer avance

---
+++ empleados.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct EMPLEADOS{
+    int numero;
+    string nombre;
+    int ventas[12];
+    float salario;
+    int suma;
+};
+
+int main(){
+    
+}
