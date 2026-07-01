20c1092a744b3741a957cf5116031858fe292200
zroquec <zroquec@unjbg.edu.pe>
2026-07-01 10:16:33 -0500

    Empleado: Segundo avance

--- empleados.cpp
+++ empleados.cpp
@@ -10,5 +10,24 @@ struct EMPLEADOS{
 };
 
 int main(){
-    
+    EMPLEADOS E[50];
+    int n,max;
+    cin>>n;
+    cin.ignore();
+
+    for(int i=0; i<n; i++){
+        cout<<"\nEMPLEADO "<<i+1<<endl;
+        cout<<"Nombre: ";
+        getline(cin, E[i].nombre);
+        cout<<"Nro de empleado: ";
+        cin>>E[i].numero;
+        cout<<"Salario del empleado: ";
+        cin>>E[i].salario;
+        for(int j=0; j<12; j++){
+            cout<<"Nro de ventas "<<"mes "<<j+1<<": ";
+            cin>>E[i].ventas[j];
+        }
+        cin.ignore();
+    }
+
 }
