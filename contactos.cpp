0c56a40aebc93025d49621c190517ef428d0550c
zroquec <zroquec@unjbg.edu.pe>
2026-07-04 12:06:59 -0500

    contactos: primer avance

---
+++ contactos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct ContactoEmail{
+    string nombre;
+    string sexo;
+    int edad;
+    string email;
+};
+
+int main(){
+    
+}
