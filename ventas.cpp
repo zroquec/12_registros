a36c8983b624d015c0c3e5d93351bf20ae9aeaea
zroquec <zroquec@unjbg.edu.pe>
2026-07-05 16:11:23 -0500

    ventas: avance 1

---
+++ ventas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Producto{
+    string nombre;
+    float precio;
+};
+
+struct Venta{
+    int idVenta;
+    string producto;
+    int cantidad;
+    float precioTotal;
+};
+
+int main(){
+    
+}
